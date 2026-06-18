74f91391701a2b9adc1682b695abd47fe8861267
UrielDA05 <dejesusatenogenesuriel@gmail.com>
2026-06-17 19:29:20 -0600

    Delete Proyecto notas directory

--- Proyecto notas/js/salir.js
+++
@@ -1,6 +0,0 @@
-const btnSalir = document.getElementById("btn-salir");
-
-btnSalir.addEventListener("click",()=>{
-    localStorage.removeItem("usuario")
-    window.location.href = "index.html";
-})
